6a3efd84b4d015652969acf7654eaa206a583ed0
xiaolongren <52064463+Little-Dragon-God@users.noreply.github.com>
2021-06-16 22:28:56 +0800

    队列实现

--- src/main/java/com/xls/arr/QueueDemo.java
+++ src/main/java/com/xls/arr/QueueDemo.java
@@ -63,7 +63,7 @@ class ArrayQueue {
         maxSize = arrMaxSize;
         arr = new int[maxSize];
         front = -1;//指向队列头部，分析出front是指向队列头的前一个位置
-        rear = -1;//指向队列尾
+        rear = -1;//指向队列尾，指向队列的最后一个元素
     }
 
     //判断队列是否满了
